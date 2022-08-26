9155caed984be396ba110641cccb2fca3c5eefab
Martin Padron <martinepadronv@gmail.com>
2022-08-26 18:31:00 -0400

    Agregando el !important para ver si se solventa el color del bullet de una buena vez

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import "swiper/css/pagination";
-import '../styles/globals.scss'
-import "swiper/css";
+import "../styles/globals.scss";
+import "swiper/scss";
+import "swiper/scss/pagination";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
