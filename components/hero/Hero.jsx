1431ec74cf2e70ca8e4eb8aa13408e29d4364e50
Martin Padron <martinepadronv@gmail.com>
2022-09-23 09:44:02 -0400

    Modificando Hero

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -6,33 +6,21 @@ import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <section className={style.heroSection}>
+    <section className={style.section}>
       <div className={style.banner}>
-        <div className={style.titles}>
-          <h1>
-            Martín <br /> Padrón
-          </h1>
-          <h2>Desarrollador Front-end</h2>
+        <div className={style.content}>
+          <h1>Martín Padrón</h1>
+          <h2>Desarrollador Frontend y diseñador UI/UX</h2>
           <Link href="#contacto"> Hablemos </Link>
         </div>
-
-        <div className={style.img_container}>
-          <Image src={yo} alt="Martin Padrón" />
+        <div className={style.foto}>
+          <Image src={yo} alt="Martin Padron Desarrollador web" />
         </div>
       </div>
 
-      <Socials />
-
-      <div className={style.conoceme}>
-        <Link href="/Sobre-mi"> Conóceme </Link>
+      <div className={style.linker}>
+        <Link href="/Sobre-mi"> Más de mi </Link>
       </div>
-
-      <hr className={style.separator}/>
-
-      <div className={style.intro}>
-        <p>Hola! Encantado de tenerte por aquí, espero que tu experiencia por acá sea de lo más agradable... Si quieres saber más de mi dale  <Link href="/Sobre-mi">click a este enlace</Link></p>
-      </div>
-
     </section>
   );
 };
