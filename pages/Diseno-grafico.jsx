7f730ad236197c83f8acaba1247b45bbd1d9c50c
Martin Padron <martinepadronv@gmail.com>
2022-10-12 12:42:24 -0400

    Chetando la sección de marketing

--- pages/Diseno-grafico.jsx
+++ pages/Diseno-grafico.jsx
@@ -52,70 +52,75 @@ const Diseno_grafico = () => {
 
       <C1
         foto={illus}
-        title="Diseño gráfico, branding y creación de logos profesionales. Le damos vida a tus ideas"
+        title="Diseño gráfico, branding y creación de logos profesionales."
         content="Desde el logo de tu empresa hasta revistas, folletos, vallas, banners y toda la imagen que involucre una campaña de mercadeo la hacemos realidad para ti. Una buena imagen es fundamental en cualquier empresa."
       />
 
-      <Contenido title="¿Qué es el diseño gráfico?">
-        <p>
-          <strong>¿Qué es el diseño gráfico?</strong> Muchas personas tienen una
-          idea muy básica acerca de qué hace un diseñador gráfico. ¿Crean
-          logotipos para empresas? ¿Producen anuncios para internet y revistas?
-          ¿Editan imágenes con Photoshop?{" "}
-        </p>
-        <p>
-          Según el American Institute of Graphic Arts (AIGA), el diseño gráfico
-          se define como el arte y la práctica de planificar y proyectar ideas y
-          experiencias con contenido visual y textual. En otras palabras, el
-          diseño gráfico comunica ciertas ideas o mensajes de manera visual.
-          Estos gráficos pueden ser tan simples como un logotipo de negocio o
-          tan complejos como diseños de página en un sitio web.
-        </p>
-        <p>
-          En un concepto amplio, el término diseño gráfico designa superficies
-          bidimensionales materiales o virtuales (generadas por computadora),
-          medios de comunicación visuales y soportes de información mediante la
-          tipografía, la imagen, el color y el material. Asimismo, el diseño
-          gráfico es un medio para representar visualmente el lenguaje y los
-          pensamientos, así como para hacer visibles, legibles y comunicables
-          las conexiones teóricas. Hoy en día, el diseño gráfico es un mediador
-          en casi todos los ámbitos de la vida.
-        </p>
-      </Contenido>
+      <section>
+        <Contenido title="¿Qué es el diseño gráfico?">
+          <p>
+            <strong>¿Qué es el diseño gráfico?</strong> Muchas personas tienen
+            una idea muy básica acerca de qué hace un diseñador gráfico. ¿Crean
+            logotipos para empresas? ¿Producen anuncios para internet y
+            revistas? ¿Editan imágenes con Photoshop?{" "}
+          </p>
+          <p>
+            Según el American Institute of Graphic Arts (AIGA), el diseño
+            gráfico se define como el arte y la práctica de planificar y
+            proyectar ideas y experiencias con contenido visual y textual. En
+            otras palabras, el diseño gráfico comunica ciertas ideas o mensajes
+            de manera visual. Estos gráficos pueden ser tan simples como un
+            logotipo de negocio o tan complejos como diseños de página en un
+            sitio web.
+          </p>
+          <p>
+            En un concepto amplio, el término diseño gráfico designa superficies
+            bidimensionales materiales o virtuales (generadas por computadora),
+            medios de comunicación visuales y soportes de información mediante
+            la tipografía, la imagen, el color y el material. Asimismo, el
+            diseño gráfico es un medio para representar visualmente el lenguaje
+            y los pensamientos, así como para hacer visibles, legibles y
+            comunicables las conexiones teóricas. Hoy en día, el diseño gráfico
+            es un mediador en casi todos los ámbitos de la vida.
+          </p>
+        </Contenido>
 
-      <Contenido title="¿Por qué es importante el diseño gráfico?">
-        <p>
-          Partiendo de la definición y la descripción de qué es el diseño
-          gráfico de antes, es clave entender la importancia del diseño gráfico
-          hoy en día, ya que esta puede ayudar a alcanzar todo tipo de objetivos
-          mejorando la comunicación con otras personas.{" "}
-        </p>
-        <p>
-          El ámbito de cosas que se pueden crear aprendiendo los fundamentos del
-          diseño gráfico es enorme. Va desde presentaciones de empresa, pasando
-          por folletos sencillos hasta <strong>modernos sitios web.</strong> El
-          diseño gráfico te permite, por ejemplo:
-        </p>
-        <ul>
-          <li>Causar una buena primera impresión.</li>
-          <li>Distinguirte de la competencia.</li>
-          <li>Proyectar una imagen de profesionalismo y confianza.</li>
-          <li>
-            Hacer que tus ideas tengan mayor alcance y resonancia a través del
-            lenguaje y la comunicación visual.
-          </li>
-        </ul>
-        <p>
-          En resumen, el diseño gráfico es un arte muy poderoso que juega un
-          papel destacado en áreas tan importantes como el marketing digital.
-        </p>
-        <p>
-          Las empresas de éxito lo utilizan una y otra vez para dar a su perfil
-          y a su marca una expresión contemporánea y moderna. Sin embargo, los
-          fundamentos del diseño gráfico no solo son relevantes para fines
-          comerciales, sino también en el campo del arte y la pintura.
-        </p>
-      </Contenido>
+        <Contenido title="¿Por qué es importante el diseño gráfico?">
+          <p>
+            Partiendo de la definición y la descripción de qué es el diseño
+            gráfico de antes, es clave entender la importancia del diseño
+            gráfico hoy en día, ya que esta puede ayudar a alcanzar todo tipo de
+            objetivos mejorando la comunicación con otras personas.{" "}
+          </p>
+          <p>
+            El ámbito de cosas que se pueden crear aprendiendo los fundamentos
+            del diseño gráfico es enorme. Va desde presentaciones de empresa,
+            pasando por folletos sencillos hasta{" "}
+            <strong>modernos sitios web.</strong> El diseño gráfico te permite,
+            por ejemplo:
+          </p>
+          <ul>
+            <li>Causar una buena primera impresión.</li>
+            <li>Distinguirte de la competencia.</li>
+            <li>Proyectar una imagen de profesionalismo y confianza.</li>
+            <li>
+              Hacer que tus ideas tengan mayor alcance y resonancia a través del
+              lenguaje y la comunicación visual.
+            </li>
+          </ul>
+          <p>
+            En resumen, el diseño gráfico es un arte muy poderoso que juega un
+            papel destacado en áreas tan importantes como el marketing digital.
+          </p>
+          <p>
+            Las empresas de éxito lo utilizan una y otra vez para dar a su
+            perfil y a su marca una expresión contemporánea y moderna. Sin
+            embargo, los fundamentos del diseño gráfico no solo son relevantes
+            para fines comerciales, sino también en el campo del arte y la
+            pintura.
+          </p>
+        </Contenido>
+      </section>
 
       <C2
         icon1={<BiShapeSquare className="icon" />}
--- pages/Marketing.jsx
+++ pages/Marketing.jsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Header from "../components/header/Header";
+import style from "../page_components/contenido/Contenido.module.scss";
 
 import C1 from "../page_components/c1/C1";
 import illus from "../public/img/illustrations/marketing_illus.svg";
+
+import Contenido from "../page_components/contenido/Contenido";
 
 import C2 from "../page_components/c2/C2";
 import { AiOutlineUser } from "react-icons/ai";
@@ -22,10 +25,19 @@ const Marketing = () => {
   return (
     <>
       <Head>
-        <title>Marketing | Martín Padrón</title>
+        <title>
+          Marketing digital y gestión de redes sociales en Venezuela | Martín
+          Padrón
+        </title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
-        <meta name="description" content="Marketing digital para que empieces a ganar clientes a traves de internet con las mejores estrategias adaptadas a las necesidades de tu marca o empresa." />
-        <meta name="keywords" content="marketing, marketing digital, instagram, facebook, tiktok, community manager, social media" />
+        <meta
+          name="description"
+          content="Marketing digital para que empieces a ganar clientes a traves de internet con las mejores estrategias adaptadas a las necesidades de tu marca o empresa."
+        />
+        <meta
+          name="keywords"
+          content="marketing, marketing digital, instagram, facebook, tiktok, community manager, social media"
+        />
         <meta
           property="og:image"
           content="https://www.martineduardo.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fog_image.d303d898.jpg&w=3840&q=75"
@@ -41,9 +53,98 @@ const Marketing = () => {
 
       <C1
         foto={illus}
-        title="Gestión de Redes sociales"
-        content="Conoceremos a tu audiencia y estableceremos objetivos claros para crear contenido relevante, que entretenga y que sea frecuente. Además de medir los resultados de todas las redes sociales para fidelizar tus usuarios."
+        title="Marketing digital y gestión de redes sociales, estrategias adaptadas a las neceidades de tu marca "
+        content="Conoceremos a tu audiencia y estableceremos objetivos claros para crear contenido relevante, que entretenga y que sea frecuente. Además de medir los resultados de todas las redes sociales para fidelizar tus usuarios y generar conversiones."
       />
+
+      <section>
+        <Contenido title="¿Qué es el marketing digital?">
+          <p>
+            El Marketing Digital, también llamado mercadotecnia online o
+            mercadeo en línea, se define como una forma de marketing que utiliza
+            los nuevos medios y canales publicitarios tecnológicos y digitales
+            como internet o el móvil. Gracias al poder de la tecnología, permite
+            crear experiencias únicas y personalizadas para medir y registrar
+            todo lo que ocurre con el objetivo de mejorar la experiencia del
+            usuario.
+          </p>
+        </Contenido>
+
+        <Contenido title="Ventajas del marketing digital">
+          <p>
+            Debido a las características que internet aporta al Marketing
+            Digital podemos atribuirle claramente las siguientes ventajas:
+          </p>
+          <ul className={style.marketing_list}>
+            <li>
+              <strong>Global:</strong> Una vez posicionado tu producto en
+              internet, cualquier persona de cualquier parte del mundo podrá
+              acceder a él.
+            </li>
+            <li>
+              <strong>Interactivo:</strong> Internet es un canal de comunicación
+              bidireccional por lo tanto el Marketing Digital también lo es. De
+              esta manera, la comunicación entre empresas y consumidores se ha
+              maximizado sobre todo desde la irrupción de las RRSS.
+            </li>
+            <li>
+              <strong>Perfectamente medible:</strong> Los datos ya no se basan
+              en estudios de mercado, lentos de realizar y de elevado coste.
+              Ahora, se recogen de manera inmediata a través del seguimiento de
+              las acciones que el usuario realiza directamente en internet.
+              Permite obtener datos personales, de preferencias y usabilidad
+              que, analizados y gestionados adecuadamente, permiten la creación
+              de acciones más eficientes.
+            </li>
+
+            <li>
+              <strong>Hiper segmentable:</strong> Gracias a la gran cantidad de
+              información recogida sobre los usuarios de internet, es posible
+              diversificar mucho más las acciones de marketing. Podemos apuntar
+              a objetivos cada vez más perfilados y definidos. Hoy en día,
+              segmentamos las campañas incorporando nuevas variables
+              geográficas, horarias y en función del dispositivo de acceso.
+            </li>
+          </ul>
+        </Contenido>
+
+        <Contenido title="diferencias entre marketing digital y diseño gráfico">
+          <p>
+            En vista de la importancia que tienen ambas en la difusión de la
+            información y dar a conocer un producto o servicio, es indispensable
+            conocer sus diferencias. Aunque son disciplinas muy relacionadas,
+            existen aspectos que las diferencian entre sí dentro de los cuales
+            se pueden mencionar:
+          </p>
+          <ul className={style.diferencias_list}>
+            <li>
+              Las estrategias de diseño gráfico conforman una composición única
+              con el fin de transmitir un mensaje visual con múltiples
+              objetivos. Por su parte, las estrategias de marketing tienen como
+              finalidad mantenerse comunicado con sus clientes para generar
+              ganancias.
+            </li>
+            <li>
+              El marketing digital tiene como fundamento los estudios de
+              mercado, los análisis y estrategias anteriores para lograr
+              posición de la marca. El diseño se basa en la creatividad para
+              formular conceptos para llegar a un público determinado.
+            </li>
+            <li>
+              El publicista usa las ventajas del marketing tradicional para
+              integrarlas con estrategias digitales y así lograr resultados. La
+              función del diseño recae sobre la figura del diseñador quien
+              combina distintas herramientas visuales, digitales, de forma,
+              color y tamaño para lograr la composición.
+            </li>
+            <li>
+              El diseño gráfico busca generar un cambio en el pensamiento para
+              promover una idea o vender un producto. Las estrategias digitales
+              buscan visibilidad y ganancias en ventas.
+            </li>
+          </ul>
+        </Contenido>
+      </section>
 
       <C2
         icon1={<AiOutlineUser className="icon" />}
@@ -60,18 +161,27 @@ const Marketing = () => {
         servi4="Estrategia"
         content4="Nos enfocaremos en la creación y distribución de contenido relevante y
         de calidad, para dotar de valor y significado a la marca."
+        title="Nuestros servicios"
       />
 
       <Title2
-        small="Google, instagram, facebook..."
-        title="Lleva tu negocio a lo más alto"
+        small="Lleva tu negocio a lo más alto"
+        title="Pasos y metodología de trabajo"
       />
 
       <GridMarketing />
 
-      <Contacto title="¿Comenzamos?" content="No esperes más y empieza a ganar clientes a través de internet" />
+      <Contacto
+        title="¿Comenzamos?"
+        content="No esperes más y empieza a ganar clientes a través de internet"
+      />
 
-      <Footer servicios="/#servicios" experiencia="/#experiencia" portfolio="/Portfolio" contacto="#contacto" />
+      <Footer
+        servicios="/#servicios"
+        experiencia="/#experiencia"
+        portfolio="/Portfolio"
+        contacto="#contacto"
+      />
     </>
   );
 };
