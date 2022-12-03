a13e334e7e931abfa81bb7f318a3dbe50caece04
Martin Padron <martinepadronv@gmail.com>
2022-12-03 08:57:08 -0400

    Hero listo

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -4,9 +4,14 @@ import liquid from "../../public/img/liquid.jpg";
 import collage from "../../public/img/collage.jpg";
 import logo from "../../public/img/mp_logo.svg";
 
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineWhatsApp } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
 const Hero = () => {
   return (
     <section className="flex lg:h-screen">
+      {/* Aside  */}
       <div className="hidden lg:flex flex-col items-center justify-between bg-primary-100 w-72 py-8">
         <div className="flex flex-col items-center gap-2">
           <Image src={logo} alt="martin padron" className="w-20" />
@@ -14,33 +19,72 @@ const Hero = () => {
         </div>
 
         <div className="flex flex-col gap-2 items-center">
-          <Link href="/">
-            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105">Servicios</button>
+          <Link href="/#servicios">
+            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105">
+              Servicios
+            </button>
           </Link>
 
-          <Link href="/">
-            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105">Cotizaciones</button>
+          <Link href="/precios">
+            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105">
+              Cotizaciones
+            </button>
           </Link>
 
-          <Link href="/">
-            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105">Sobre mi</button>
+          <Link href="/sobre-mi">
+            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105">
+              Sobre mi
+            </button>
           </Link>
         </div>
 
         <div>
-          <Link href='/'>
-            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105 border border-white">Asesoría GRATIS</button>
+          <Link href="/">
+            <button className="bg-primary-200 p-3 w-[11rem] rounded-lg transition-all hover:scale-105 border border-white">
+              Asesoría GRATIS
+            </button>
           </Link>
+
+          <div className="flex gap-7 justify-center mt-5">
+            <a
+              href="https://www.github.com/maartou"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub className="text-3xl" />
+            </a>
+
+            <a
+              href="https://www.instagram.com/programarto/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineInstagram className="text-3xl" />
+            </a>
+
+            <a
+              href="https://wa.me/+584144245029"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineWhatsApp className="text-3xl" />
+            </a>
+          </div>
         </div>
       </div>
 
       <div className="w-full lg:grid lg:grid-rows-2">
         {/* Foto principal y H1  */}
-        <div className="relative h-[35rem] lg:h-auto flex justify-center items-center px-4">
+        <div className="relative h-[35rem] lg:h-auto flex justify-center items-center px-4 saturate-0">
           <h1 className="z-10 uppercase font-bold text-5xl leading-[3rem] lg:text-7xl ">
             Diseño y Desarrollo web
           </h1>
-          <Image src={liquid} alt="" fill className="object-cover -z-10" />
+          <Image
+            src={liquid}
+            alt=""
+            fill
+            className="object-cover -z-10 brightness-[0.4]"
+          />
         </div>
 
         {/* Portfolio y botones  */}
@@ -62,7 +106,7 @@ const Hero = () => {
           </div>
 
           <div className="bg-black text-white flex justify-center items-center py-20 lg:order-1">
-            <Link href="/">
+            <Link href="/#experiencia">
               <button className="uppercase tracking-widest font-light border py-2 px-8  text-xl hover:bg-white hover:text-black transition-all">
                 Skills
               </button>
@@ -70,7 +114,7 @@ const Hero = () => {
           </div>
 
           <div className="bg-white text-black flex justify-center items-center py-20 lg:order-3">
-            <Link href="/">
+            <Link href="/#contacto">
               <button className="uppercase tracking-widest font-light border border-black py-2 px-8  text-xl hover:bg-black hover:text-white transition-all">
                 Contacto
               </button>
