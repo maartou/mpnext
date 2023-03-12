1257958ec3c28a0772eed379f27b280578cf5362
Martin Padron <martinepadronv@gmail.com>
2023-03-12 09:44:38 -0400

    Quitando los redirects

--- next.config.js
+++ next.config.js
@@ -2,20 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: '/Desarrollo-web',
-        destination: '/desarrollo-web',
-        permanent: true,
-      },
-      {
-        source: '/Portfolio',
-        destination: '/portfolio',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
